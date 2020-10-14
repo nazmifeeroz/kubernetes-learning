5320fb35c4bd47b4aa77da34204c9c80e9d8847a
Muhammad Nazmi Bin Feeroz <nazmifeeroz@ntuclink.com.sg>
2020-10-15 07:13:35 +0800

    :art: refactors useEffect function

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -3,16 +3,16 @@ import React, { useState, useEffect } from 'react'
 export default ({ postId }) => {
   const [comments, setComments] = useState([])
 
-  const fetchData = async () => {
-    const res = await fetch(`http://localhost:4001/posts/${postId}/comments`)
-    const data = await res.json()
-
-    setComments(data)
-  }
-
   useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(`http://localhost:4001/posts/${postId}/comments`)
+      const data = await res.json()
+
+      setComments(data)
+    }
+
     fetchData()
-  }, [])
+  }, [postId])
 
   const renderedComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>
