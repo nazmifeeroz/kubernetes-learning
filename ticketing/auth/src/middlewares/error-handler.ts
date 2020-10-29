2230a96a71d0bb3da77b099ef6866d39a308d833
Nazmi Feeroz <nazmifeeroz@hotmail.com>
2020-10-29 10:57:15 +0800

    :sparkles: build request and database validation errors

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from 'express'
+import { RequestValidationError } from '../errors/request-validation-error'
+import { DatabaseConnectionError } from '../errors/database-connection-error'
 
 export const errorHandler = (
   err: Error,
@@ -6,9 +8,30 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log('Something went wrong', err)
+  if (err instanceof RequestValidationError) {
+    const formattedError = err.errors.map((error) => ({
+      message: error.msg,
+      field: error.param,
+    }))
+
+    return res.status(400).send({ errors: formattedError })
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(500).send({
+      errors: [
+        {
+          message: err.reason,
+        },
+      ],
+    })
+  }
 
   res.status(400).send({
-    message: 'Something went wrong',
+    errors: [
+      {
+        message: 'Something went wrong',
+      },
+    ],
   })
 }
